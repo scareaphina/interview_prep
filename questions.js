b9913ed476cf5bfb60035df78fca2d3aa85f1e05
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-06 10:47:16 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,5 @@
+// finding a missing number in an integer array
+
 var numArray = [1, 2, 4, 5, 6, 7];
 var mia = [];
 
@@ -13,3 +15,29 @@ for (var i = 1; i < numArray.length; i++) {
 }
 
 console.log(mia);
+
+
+// finding duplicates in an integer array
+
+function findDuplicates(data) {
+
+  let result = [];
+
+  data.forEach(function(element, index) {
+
+  // find if there is a duplicate or not
+  if (data.indexOf(element, index + 1) > -1) {
+
+    // find if the element is already in the result array or not
+    if (result.indexOf(element) === -1) {
+      result.push(element);
+    }
+  }
+});
+
+return result;
+}
+
+console.log(findDuplicates([]));
+console.log(findDuplicates([1, 1, 1]));
+console.log(findDuplicates([1, 2, 3, 1, 2, 1]));
