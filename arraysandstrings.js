2016d314d858f5fae417fcb8ccacd5e591939002
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-27 18:07:16 -0500

    Update arraysandstrings.js

--- arraysandstrings.js
+++ arraysandstrings.js
@@ -168,3 +168,40 @@ export function rotateMatrix(matrix) {
   return matrix;
 }
 
+////////
+// 1.8
+// write an algorithm such that if an element in an MxN matrix is 0, its entire row and columns are set to 0
+
+export function zeroMatrix(matrix) {
+  if (!matrix) {
+    throw new Error("invalid matrix");
+  }
+  if (matrix.length === 0) {
+    return matrix;
+  }
+
+  let rows = new Array(matrix.length),
+  cols = new Array(matrix[0].length);
+
+  rows.fill(false);
+  cols.fill(false);
+
+  for (let y = 0; y < rows.length; ++y) {
+    for (let x = 0; x < cols.length; ++x) {
+      if (matrix[y][x] === 0) {
+        rows[y] = true;
+        cols[x] = true;
+      }
+    }
+  }
+
+  for (let y = 0; y < rows.length; ++y) {
+    for (let x = 0; x < cols.length; ++x) {
+      if (rows[y] || cols[x]) {
+        matrix[y][x] = 0;
+      }
+    }
+  }
+
+  return matrix;
+}
