e0bf71e95953cca18b287bd6e96768ca48f397fc
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-27 16:50:47 -0500

    Update arraysandstrings.js

--- arraysandstrings.js
+++ arraysandstrings.js
@@ -130,3 +130,41 @@ function compressString(str) {
 
 console.log(compressString("aabcccccaaa"));
 console.log(compressString("banana"));
+
+////////
+// 1.7
+// given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. can you do this in place?
+
+export function rotateMatrix(matrix) {
+  if (!matrix || matrix.length === 0 || matrix.length !== matrix[0].length) {
+    throw new error("invalid matrix");
+  }
+  if (matrix.length < 2) {
+    return matrix;
+  }
+
+  let len = matrix.length - 1,
+    half = Math.floor(matrix.length / 2);
+
+  for (let start = 0; start < half; ++start) {
+
+    for (let i = 0; i < len - (start * 2); ++i) {
+      let y = start,
+        x = start + i,
+        prev = matrix[y][x];
+
+      for (let j = 0; j < 4; ++j) {
+        let nextY = x,
+          nextX = len - y,
+          next = matrix[nextY][nextX];
+        matrix[nextY][nextX] = prev;
+        prev = next;
+        x = nextX;
+        y = nextY;
+      }
+    }
+  }
+
+  return matrix;
+}
+
