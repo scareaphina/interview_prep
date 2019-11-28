a3cf5a03b7cfc17b0a420e7e4c2dea2d9a4093fd
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-27 20:43:49 -0500

    Update ch2_linked_lists.js

--- ch2_linked_lists.js
+++ ch2_linked_lists.js
@@ -26,4 +26,29 @@ export function removeDuplicatesSent(list) {
 
 ////////
 // 2.2
-// walk one pointer ahead k nodes first then create a second pointer to the start of the list. walk both pointers until the first one hits the end of the list, at that point the second pointer will be pointing to the kth to the last node 
+// walk one pointer ahead k nodes first then create a second pointer to the start of the list. walk both pointers until the first one hits the end of the list, at that point the second pointer will be pointing to the kth to the last node
+
+export function kthToLastTwoPointers(list, k) {
+  if (!list) {
+    throw new Error("invalid list");
+  }
+
+  let last = list,
+  kth = list;
+
+  for (let i = 0; i < k; ++i) {
+    last = last.next;
+    if (!last) {
+      throw new Error("list is not long enough");
+    }
+  }
+
+  while (last.next) {
+    last = last.next;
+    kth = kth.next;
+  }
+
+  return kth.val;
+}
+
+// determine the length of the input list. subtract k from the calculated length and skip that many nodes from the start of the list. that node is the kth to last.
