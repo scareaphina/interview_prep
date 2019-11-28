7b6da7062dbc1ba55dcd951231e1ff606a9653b4
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-27 19:27:21 -0500

    Update ch2_linked_lists.js

--- ch2_linked_lists.js
+++ ch2_linked_lists.js
@@ -3,5 +3,23 @@
 // iterate through list keeping a set of all the values seen. if a seen value is seen again in the list then skip over it
 
 export function removeDuplicatesSent(list) {
-  
+  if (!list) {
+    return list;
+  }
+
+  let seen = new Set(),
+  node = list;
+
+  seen.add(node.val);
+  while (node.next) {
+    if (seen.has(node.next.val)) {
+      node.next = node.next.next;
+    }
+    else {
+      seen.add(node.next.val);
+      node = node.next;
+    }
+  }
+
+  return list;
 }
